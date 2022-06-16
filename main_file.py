0eda60cf2e858e9e061e72692c88dec4b3cd9b06
Colin O'Brien <cmobrien123@gmail.com>
2022-06-16 15:21:16 -0400

    adding requirements.txt

--- main_file.py
+++ main_file.py
@@ -2,7 +2,6 @@ import streamlit as st
 # SETTING PAGE CONFIG TO Centered MODE
 st.set_page_config(layout="wide")
 
-
 st.title("""
 Anomaly Detection of Electrical Signals in a Particle Accelerator
 """)
